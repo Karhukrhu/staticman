0d5dd078092be3fc9ec292cb7c5efd09e738fef3
Karhukrhu <karhukrhu@gmail.com>
2026-09-18 21:19:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 try {
   const StaticmanAPI = require('./server')
   const api = new StaticmanAPI()
-
-  module.exports = app;
   
   api.start(port => {
     console.log('Staticman API running on port', port)
