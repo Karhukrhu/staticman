a7be00a62c7ce2a2d3c3fc73a305eb0820eb22d4
Karhukrhu <karhukrhu@gmail.com>
2026-09-18 21:28:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-try {
-  const StaticmanAPI = require('./server')
-  const api = new StaticmanAPI()
-  
+const StaticmanAPI = require('./server')
+const api = new StaticmanAPI()
+
+// 1. Export the Express app so Vercel can handle HTTP requests
+module.exports = api.app
+
+// 2. Only start the traditional server if we are NOT on Vercel
+// (This allows you to still run it locally with `node index.js` if you ever need to)
+if (process.env.VERCEL !== '1') {
   api.start(port => {
     console.log('Staticman API running on port', port)
   })
-  
-} catch (e) {
-  console.error(e)
 }
